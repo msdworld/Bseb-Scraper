8cce620b2968ef3e8c37c7f88bc33c2506603404
msdworld <75013420+msdworld@users.noreply.github.com>
2026-03-27 08:57:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ const FIRST_CHECK_LIMIT = 100;
 const CONTINUOUS_FAIL_LIMIT = 20;
 
 // Speed
-const CONCURRENCY = 1000;
-const BATCH_SIZE = 4000;
+const CONCURRENCY = 500;
+const BATCH_SIZE = 1000;
 const REQUEST_TIMEOUT = 5000;
 
 // Save
