d96d9ce740f3b48ff3b3f6b8dba254c494f933ea
msdworld <75013420+msdworld@users.noreply.github.com>
2026-04-06 00:15:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ const SAVE_EVERY_VALID_RESULTS = 100;
 // ===============================
 // SPLIT RANGE (CHANGE THIS EACH RUN)
 // ===============================
-const START_INDEX = 2001;
-const END_INDEX = 3000;
+const START_INDEX = 3001;
+const END_INDEX = 5000;
 
 // ===============================
 // AXIOS CLIENT
