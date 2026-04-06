68b05839a1ebf9a82ca309f91d05cc08b5836c75
msdworld <75013420+msdworld@users.noreply.github.com>
2026-04-06 08:46:17 +0530

    Update script.js

--- BSEB 10TH Result/script.js
+++ BSEB 10TH Result/script.js
@@ -22,13 +22,13 @@ const BATCH_SIZE = 100;
 const REQUEST_TIMEOUT = 5000;
 
 // SAVE
-const SAVE_EVERY_VALID_RESULTS = 200;
+const SAVE_EVERY_VALID_RESULTS = 300;
 
 // ===============================
 // SPLIT RANGE (CHANGE EACH RUN)
 // ===============================
-const START_INDEX = 5001;
-const END_INDEX = 5050;
+const START_INDEX = 5050;
+const END_INDEX = 7000;
 
 // ===============================
 // AXIOS CLIENT
