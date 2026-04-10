6a6986c4a195695012f91526a57784acc12720d3
msdworld <75013420+msdworld@users.noreply.github.com>
2026-04-10 20:54:21 +0530

    Update script.js

--- BSEB 12TH RESULT/Bseb 12th Result 2026/script.js
+++ BSEB 12TH RESULT/Bseb 12th Result 2026/script.js
@@ -17,8 +17,8 @@ const BASE_DIR = __dirname;
 // ===============================
 // EDIT ONLY THESE 2 EVERY RUN
 // ===============================
-const DISTRICT_PREFIX = "83";
-const OUTPUT_FILE_NAME = "khagaria-83-bseb-12th-full-result-2026.json";
+const DISTRICT_PREFIX = "93";
+const OUTPUT_FILE_NAME = "kishanganj-93-bseb-12th-full-result-2026.json";
 
 // ===============================
 // FILE PATHS
